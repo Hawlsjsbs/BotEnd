fac721e4c3fb9ffef5d084cc3d85ad502950db21
Hawlsjsbs <91583891+Hawlsjsbs@users.noreply.github.com>
2021-10-10 00:14:22 +0200

    Update bott.py

--- bott.py
+++ bott.py
@@ -1,11 +1,6 @@
-import telebot
-token = "2065968320:AAHuVBolVHIoDE2bAmjVg_Ssjfa-CQgIqY8"
-bot = telebot.TeleBot(token, parse_mode=None)
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
 
-bot.polling(True)
+
+
 from telegram.ext import *
 import requests
 import json
